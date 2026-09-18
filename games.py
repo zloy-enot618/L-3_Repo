60f3b823a44a78a2561dc02e726e048e772a6fc8
Oleg Markin <o51393680@gmail.com>
2026-09-18 19:54:50 +0300

    Проект готов

--- games.py
+++ games.py
@@ -1,4 +1,7 @@
+import time
 def show_games(games):
     print("=== Каталог настольных игр ===")
     for number, game in enumerate(games, start=1):
-        print(f"{number}. Название: {game['name']}\n[!] Жанр: {game['genre']}")
+        print(f"{number}. Название: {game['name']}\n[!] Жанр: {game['genre']}\n")
+
+        time.sleep(0.3)
